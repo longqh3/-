d98da9589784a17d55875d8196d2998b4f8f8870
yuanyy06 <yuanyy06@outlook.com>
2020-10-10 13:44:53 +0800

    update codes

---
+++ 网页信息提取/doi_extract_download.py
@@ -0,0 +1,34 @@
+###
+# 2019研究生学术道德规范在线考试网页信息提取
+###
+
+from pyquery import PyQuery as pq
+
+from urllib.request import urlopen
+
+from bs4 import BeautifulSoup
+
+path = "C:\\Users\\qihan\\Desktop\\Issues _ Age and Ageing _ Oxford Academic.html"
+htmlfile = open(path, 'r', encoding='utf-8')
+htmlhandle = htmlfile.read()
+
+soup = BeautifulSoup(htmlhandle)
+
+allList = soup.find_all("dl", {"class":"questionsContent"})
+
+list = list(range(1,26)) + list(range(1,26))
+i = 0
+
+for Q_A in allList:
+    questions = Q_A.find_all("span",{"class":"qq"})
+    answers = Q_A.find_all("a", {"class":"words"})
+
+    print("\n", list[i], ":", questions[0].get_text())
+    i += 1
+    for answer in answers:
+        print(answer.get_text())
+
+
+# for name in nameList:
+#     print("",name.get_text())
+
